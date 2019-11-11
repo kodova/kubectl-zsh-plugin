e9f3b44f7e284e062b89108e81aea6df7abb6369
Patrick Arnold <patrick.a@geniusmonkey.com>
2019-11-11 11:49:34 -0700

    Inital commit

---
+++ kubectl.plugin.zsh
@@ -0,0 +1,28 @@
+
+# Add completions
+if [ $commands[kubectl] ]; then
+  source <(kubectl completion zsh)
+fi
+
+
+# Utility
+alias kg='kubectl get'
+alias kdel='kubectl delete'
+alias kucx='kubectl config use-context'
+alias kgcx='kubectl config get-context'
+
+# Pods
+alias kgp='kubectl get pods'
+alias kdp='kubectl describe pods'
+
+# Deployments
+alias kgd="kubectl get deployment"
+alias kdd="kubectl describe deployment"
+
+# Service
+alias kgs="kubectl get service"
+alias kds="kubectl describe service"
+
+# Ingress
+alias kgd="kubectl get ingress"
+alias kdd="kubectl describe ingress"
